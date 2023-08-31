30c85efa9b801dfb0ef2279e36d041c9b3cb89d6
hiepnguyen223 <hiepnguyenn223@gmail.com>
2023-08-31 23:23:40 +0700

    disable lint for regex

--- src/lib/isURL.ts
+++ src/lib/isURL.ts
@@ -1,4 +1,5 @@
 function isURL(url = ''): boolean {
+    /* eslint-disable no-useless-escape */
     const urlPattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
     return !!urlPattern.test(url);
 }
